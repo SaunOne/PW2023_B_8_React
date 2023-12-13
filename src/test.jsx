753ef8ad6e8bd9257ba38ed140ba734fa9113b11
Tinar Saun <tinartinar720@gmail.com>
2023-12-13 17:52:41 +0700

    first commit

---
+++ src/test.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { GetWatchLaterContents, RemoveFromWatchLater } from "./api/apiWatchLater";
+
+function Test() {
+  const [count, setCount] = useState(0)
+
+
+    const data = () => {
+
+        return  GetWatchLaterContents()
+        .then((response) => {
+          setContents(response.data);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setIsLoading(false);
+        });
+    }
+
+  return (
+    <>
+        <button onClick={GetWatchLaterContents()}>
+            Click 
+        </button>
+        <h4></h4>
+    </>
+  )
+}
+
+export default Test
